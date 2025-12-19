8119449c82a7cedb3e424babe4db0ace02fae9d3
Maxim Cojocari <92053176+MaxCojocari@users.noreply.github.com>
2025-12-19 13:10:23 +0200

    init scraper service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ScraperService } from './scraper/scraper.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const scraperService = app.get(ScraperService);
+  console.log('ScraperService#getHello:', scraperService.getHello());
+
+  await app.close();
 }
 bootstrap();
